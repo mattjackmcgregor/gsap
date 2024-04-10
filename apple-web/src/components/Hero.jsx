cc449794101bb1b26d0c1b39f4970c32d1c0d0a7
matt mcgregor <mattjackmcgregor@gmail.com>
2024-04-10 15:15:11 +1200

    moving to github repo

---
+++ apple-web/src/components/Hero.jsx
@@ -0,0 +1,61 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState, useEffect } from "react";
+
+const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    window.innerWidth < 760
+      ? setVideoSrc(smallHeroVideo)
+      : setVideoSrc(heroVideo);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      duration: 1.5,
+    });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: 20,
+      delay: 2,
+    });
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col ">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4"></source>
+          </video>
+        </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
